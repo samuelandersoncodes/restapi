51210418778bba5b55baf842b426b803a2e5b18d
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-05 14:51:19 +0000

    fix: Import env in settings to fix error

--- ecars/settings.py
+++ ecars/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import env
+
 import os
 from pathlib import Path
 
