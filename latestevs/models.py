147bff917b2eeed0a1935014fa51ad56fa0633f3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-05 22:21:36 +0000

    feat: Create evs model

--- latestevs/models.py
+++ latestevs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Evs(models.Model):
+    make = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    effieciency = models.CharField(max_length=30)
+    top_speed = models.CharField(max_length=30)
+    battery_range = models.CharField(max_length=30)
+    fast_charge = models.CharField(max_length=20)
+    released_date = models.DateField()
