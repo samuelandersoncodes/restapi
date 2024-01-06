78cb0316a44070c511682f2860e70b0f08d99906
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 16:57:13 +0000

    chore: Configure rest framework in settings

--- ecars/settings.py
+++ ecars/settings.py
@@ -40,6 +40,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
