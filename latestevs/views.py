e8f17d8869ae37229d2c2e22d87c35114adb6845
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 17:17:17 +0000

    chore: Import the ev model for evapi view

--- latestevs/views.py
+++ latestevs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Ev
 from .serializers import EvSerializer
 
 
 def evapi(request):
     # evapi view
-    return render(request, 'evapi.html')
+    return render(request, "evapi.html")
