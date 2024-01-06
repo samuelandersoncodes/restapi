361df8697c95e10572d692bde9db5eeac17349d6
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 18:25:10 +0000

    feat: Add two fields to ev model

--- latestevs/models.py
+++ latestevs/models.py
@@ -4,6 +4,19 @@ from django.db import models
 class Ev(models.Model):
     make = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
+    average_price = CharField(max_length=20)
+    seats = models.IntegerField(
+        default=2,
+        choices=[
+            (2, "Two"),
+            (4, "Four"),
+            (5, "five"),
+            (6, "six"),
+            (7, "seven"),
+            (8, eight),
+            (9, nine),
+        ],
+    )
     efficiency = models.CharField(max_length=30)
     top_speed = models.CharField(max_length=30)
     battery_range = models.CharField(max_length=30)
