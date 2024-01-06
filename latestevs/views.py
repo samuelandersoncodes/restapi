575f40b43130db0d130597655b6db3e374edd414
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 19:31:13 +0000

    feat: Add post condition to evapi view

--- latestevs/views.py
+++ latestevs/views.py
@@ -5,10 +5,15 @@ from .serializers import EvSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view("GET", "POST")
+@api_view(["GET", "POST"])
 def evapi(request):
     # evapi view
     if request.method == "GET":
         evs = Ev.objects.all()
         serializer = EvSerializer(evs, many=True)
         return JsonResponse({"evs": serializer.data}, safe=False)
+    if request.method == "POST":
+        serializer = EvSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
