9d3c15bdda8a63490f7ca5ff2344b3b41ca50edd
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 19:22:31 +0000

    feat: Set get condition for evapi view

--- latestevs/views.py
+++ latestevs/views.py
@@ -5,8 +5,10 @@ from .serializers import EvSerializer
 from rest_framework.decorators import api_view
 
 
+@api_view("GET", "POST")
 def evapi(request):
     # evapi view
-    evs = Ev.objects.all()
-    serializer = EvSerializer(evs, many=True)
-    return JsonResponse({"evs": serializer.data}, safe=False)
+    if request.method == "GET":
+        evs = Ev.objects.all()
+        serializer = EvSerializer(evs, many=True)
+        return JsonResponse({"evs": serializer.data}, safe=False)
