b2400294ac320c55c32a23ac8b2cda71fc57b3cd
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 17:34:07 +0000

    fix: Correct efficiency to for coherence

--- latestevs/models.py
+++ latestevs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ev(models.Model):
     make = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
-    effieciency = models.CharField(max_length=30)
+    efficiency = models.CharField(max_length=30)
     top_speed = models.CharField(max_length=30)
     battery_range = models.CharField(max_length=30)
     fast_charge = models.CharField(max_length=20)
