3cadbafb882be97215d95c64bbbc581a0b7f8d0a
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-06 19:18:14 +0000

    chore: Import rest framework decorators

--- latestevs/views.py
+++ latestevs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Ev
 from .serializers import EvSerializer
+from rest_framework.decorators import api_view
 
 
 def evapi(request):
