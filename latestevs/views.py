2f7483ce34d64eda865c8978581bc3c94ca84d2e
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-21 17:59:32 +0000

    feat: Add ev details delete condition

--- latestevs/views.py
+++ latestevs/views.py
@@ -35,4 +35,6 @@ def ev_details(request, id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
