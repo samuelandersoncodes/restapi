dfa26008a556be2e9f94b1e19f833cbf02737d00
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-01-21 18:27:43 +0000

    feat: Add urlpattern format variations

--- latestevs/urls.py
+++ latestevs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.evapi, name='evapi'),
     path('latestevs/<int:id>/', views.ev_details, name='ev_details'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
